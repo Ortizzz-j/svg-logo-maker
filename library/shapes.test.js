076634780096cd5ab647a0c44c3c4ecee1e0b20d
ortiz MJ <ortizzzj99@gmail.com>
2023-05-03 20:17:21 -0700

    needs work

--- library/shapes.test.js
+++ library/shapes.test.js
@@ -3,7 +3,21 @@ const {Triangle, Circle, Square} = require('./shapes')
 describe('shape', () =>{
     it('should take the color + shape and render the corresponding render()', () => {
         const triangle = new Triangle
-        const color = ''
-        expect(triangle).toBe(color)
+        triangle.setColor('green')
+        expect(triangle.render()).toEqual(`<polygon points="150, 25 244, 182 56, 182" fill="green" />`)
+    })
+})
+describe('shape', () =>{
+    it('should take the color + shape and render the corresponding render()', () => {
+        const circle = new Circle
+        circle.setColor('green')
+        expect(circle.render()).toEqual(`<circle cx="150" cy="100" r="80" fill="green" />`)
+    })
+})
+describe('shape', () =>{
+    it('should take the color + shape and render the corresponding render()', () => {
+        const square = new Square
+        square.setColor('green')
+        expect(square.render()).toEqual(`<rect x="45" y="30" width = "200" height = "150" style = "fill:${this.color}"/>`)
     })
 })
